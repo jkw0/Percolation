02628c9bed4157e139bcbdc7532269dc1d03191d
Justyna Walkowiak <justyna.walkowiak@nokia.com>
2021-04-08 21:29:37 +0200

    save distr to file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include "Functions.hpp"
+#include "BurningMethod.hpp"
+#include "ClusterDistribution.hpp"
 using namespace std;
 
+void cleanAveFile(const string& L, const string& T)
+{
+    ofstream aveFile;
+    string name = "Ave_L" + L + "T" + T + ".txt"; 
+    aveFile.open(name, ios::out);
+    aveFile << "p  PFlow  MaxCluster\n";
+    aveFile.close();
+}
+
+void cleanDistFile(const string& p, const string& L, const string& T)
+{
+    ofstream distFile;
+    string name = "Dist_p" + p + "L" + L + "T" + T + ".txt";
+    distFile.open(name, ios::out);
+    distFile << "s  n(s,p,L)\n";
+    distFile.close();
+}
 
 int main(int argc, char *argv[])
 {
-    // if (argc < 6)
-    //     cout << "Not enough arguments ;)" << std::endl;
     string sL = argv[1];
     string sT = argv[2];
     const unsigned int L = atoi(sL.c_str());
@@ -17,11 +34,7 @@ int main(int argc, char *argv[])
     vector<vector<unsigned int>> Lattice;
     Lattice.resize(L, vector<unsigned int>(L));
     
-    ofstream ofs;
-    string name = "Ave_L" + sL + "T" + sT + ".txt"; 
-    ofs.open(name, ios::out);
-    ofs << "p  PFlow  MaxCluster\n";
-    ofs.close();
+    cleanAveFile(sL, sT);
 
     vector<int> connectionData;
     vector<int> maxClusterData;
@@ -34,6 +47,7 @@ int main(int argc, char *argv[])
     uniform_real_distribution<double> dis(0, 1);
 
     while(p0 <= pk) {
+        cleanDistFile(toStringWithPrecision(p0), sL, sT);
         cout << "p = " << p0 <<endl;
         for (int i = T; i > 0; --i) {
             cout << endl << "TRIAL = " << i << endl;
@@ -53,14 +67,15 @@ int main(int argc, char *argv[])
             // cout << "Clustered" << endl;
             // printLattice(clusteredLettice);
         }
-        normalizedDistMap = normalizedClusterDistributionData(cummulativeDistMap, T);
-        p0 += dp;
-        cout << "p0=" << p0 << ", dp=" << dp << ", pk=" << pk << endl;
+        normalizedDistMap = normalizedClusterDistributionData(cummulativeDistMap);
         double Pflow = averageValue(connectionData);
         double maxClusterAvg = averageValue(maxClusterData);
         // cout << "Pflow = " << Pflow << endl;
         cout << "Max Cluster = " << maxClusterAvg << endl;
         saveToFileAve(sL, sT, p0, Pflow, maxClusterAvg);
+        saveToFileDist(sL, sT, p0, normalizedDistMap);
+        p0 += dp;
+        cout << "p0=" << p0 << ", dp=" << dp << ", pk=" << pk << endl;
         connectionData.clear();
         maxClusterData.clear();
         clusterDistMap.clear();
@@ -68,5 +83,4 @@ int main(int argc, char *argv[])
         normalizedDistMap.clear();
         cout << endl;
     }
-
 }
