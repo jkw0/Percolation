8dbcc5afda7716dfa13a8d8566956c5ec7307a47
Justyna Walkowiak <justyna.walkowiak@nokia.com>
2021-04-10 12:34:32 +0200

    ready for opt

--- cluster.cpp
+++ cluster.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Functions.hpp"
-#include "BurningMethod.hpp"
 #include "ClusterDistribution.hpp"
 #include <chrono>
 using namespace std;
@@ -17,12 +16,10 @@ int main(int argc, char *argv[])
     const double pk = atof(input["pk"].c_str());
     vector<vector<unsigned int>> Lattice;
     Lattice.resize(L, vector<unsigned int>(L));
-    
-    cleanAveFile(sL, sT);
 
-    vector<int> connectionData;
-    vector<int> maxClusterData;
     map<unsigned int, int> clusterDistMap;
+    map<int, int> cummulativeDistMap;
+    map<int, double> normalizedDistMap;
 
     random_device rd;
     mt19937 gen(rd());
@@ -31,27 +28,26 @@ int main(int argc, char *argv[])
     const double EPSILON = 0.0000001;
     auto start = std::chrono::high_resolution_clock::now();
     while((pk - p0) >= EPSILON) {
+        cleanDistFile(toStringWithPrecision(p0), sL, sT);
         // cout << "p = " << p0 <<endl;
         for (int i = T; i > 0; --i) {
             auto initialLattice = initLattice(Lattice, p0, gen, dis);
             //printLattice(initialLattice);
-            auto burnedLattice = initialLattice;
-            int connected = burningMethod(burnedLattice) ? 1 : 0;
-            connectionData.push_back(connected);
-            //printLattice(burnedLattice);
             auto clusteredLettice = initialLattice;
             auto max_el = clusterDistribution(clusteredLettice, clusterDistMap);
-            maxClusterData.push_back(max_el);
+            if (i == T) cummulativeClusterDistribution(clusterDistMap, true);
+            if (i == 1) cummulativeDistMap = cummulativeClusterDistribution(clusterDistMap, false);
+            else cummulativeClusterDistribution(clusterDistMap, false);
+            // printLattice(clusteredLettice);
         }
-        double Pflow = averageValue(connectionData);
-        double maxClusterAvg = averageValue(maxClusterData);
-        saveToFileAve(sL, sT, p0, Pflow, maxClusterAvg);
+        normalizedDistMap = normalizedClusterDistributionData(cummulativeDistMap);
+        saveToFileDist(sL, sT, p0, normalizedDistMap);
         p0 += dp;
-        connectionData.clear();
-        maxClusterData.clear();
         clusterDistMap.clear();
+        cummulativeDistMap.clear();
+        normalizedDistMap.clear();
+        // cout << endl;
     }
-
     auto finish = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = finish - start;
     cout << "Elapsed time: " << elapsed.count() << " s\n";
